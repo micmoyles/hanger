494e3d9da108d48045741524571a3ce9dbaae876
Michael Moyles <mmoyles@raspberrypi>
2016-07-22 13:29:43 +0000

    remove unused form

--- remit.py
+++ remit.py
@@ -21,50 +21,6 @@ passwd = 'wiarreft'
 session = 'REMIT'
 hanger.start('System Frequency')
 hanger.h1('System Frequency')
-form = '''
-<form class="form-inline container" method = "get" >
-<div class="form-group">
-<table class="table table-bordered">
-    <tbody>
-        <tr>
-        <td>
-        <label for="exampleInputName2">Symbol</label>
-        <input type="text" name="symbol" class="form-control" id="exampleInputName2" placeholder="ECM5/ESM5">
-        </td>
-        <td>
-        <label for="exampleInputName2">Trade Side</label>
-        <input type="text" name="side" class="form-control" id="exampleInputName2" placeholder="B/S/T">
-        </td>
-        <td>
-        <label for="exampleInputName2">Price</label>
-        <input type="text" name="price" class="form-control" id="exampleInputName2" placeholder="123.45">
-        </td>
-        <td>
-        <label for="exampleInputName2">Size</label>
-        <input type="text" name="size" class="form-control" id="exampleInputName2" placeholder="100">
-        </td>
-        <tr>
-        <td>
-        <label for="exampleInputName2">Counterparty</label>
-        <input type="text" name="counterparty" class="form-control" id="exampleInputName2" placeholder="Brennan">
-        </td>
-        <td>
-        <label for="exampleInputName2">Trade ID</label>
-        <input type="text" name="tradeid" class="form-control" id="exampleInputName2" placeholder="ABC123">
-        </td>
-        <td>
-        <label for="exampleInputName2">Venue</label>
-        <input type="text" name="venue" class="form-control" id="exampleInputName2" placeholder="APX/CME">
-        </td>
-        </tr>
-    </tbody>
-</table>
-<button type="submit" class="btn btn-default">Submit</button>
-<button type="button" class="btn btn-info">Show Query</button>
-</div>
-</form>
-'''
-#print form
 query = 'select timestamp, freq as SystemFrequency  from frequency'
 
 hanger.showquery(query)
