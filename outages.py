53489ac97e6fc1f60bb4e1b83c85be7367239412
Michael Moyles <mmoyles@raspberrypi>
2016-07-29 22:08:11 +0000

    improve logic of outage queries

--- outages.py
+++ outages.py
@@ -4,6 +4,7 @@ import cgi, cgitb; cgitb.enable()
 import sys
 import hanger
 import MySQLdb as mdb
+import datetime
 #form = cgi.FieldStorage()
 form = cgi.SvFormContentDict()
 EventStatus	  = form.get('EventStatus', '')
@@ -12,15 +13,22 @@ AffectedUnit 	  = form.get('AffectedUnit', '')
 AssetID 	  = form.get('AssetID', '')
 EventType         = form.get('EventType', '')
 FuelType          = form.get('FuelType', '')
+showForm          = form.get('showForm', '')
 MessageHeading    = form.get('MessageHeading', '')
 sql 	  = form.get('sql', '')
 
 session = 'REMIT'
+rangeInDays = 2
 db = mdb.connect( hanger.host, hanger.user, hanger.password )
 cursor = db.cursor(mdb.cursors.DictCursor)
 cursor.execute( "use %s" % session )
-hanger.start('Outages')
-hanger.h1('Outages')
+hanger.start('REMIT Events')
+hanger.h2('Current REMIT Events')
+viewAll = '''
+<div class=container>
+<a href=outages.py?showForm=True>View all events here<a>
+</div>
+'''
 form = '''
 <form class="form-inline container" method = "get" >
 <div class="form-group">
@@ -60,8 +68,14 @@ form = '''
 </div>
 </form>
 '''
-print form
-query = 'select *  from outages'
+if showForm: print form
+now = datetime.datetime.now().strftime( '%Y-%m-%d %H:%m:%S')
+t1 = (datetime.datetime.now() - datetime.timedelta(days=rangeInDays)).strftime( '%Y-%m-%d %H:%m:%S') 
+t2 = (datetime.datetime.now() + datetime.timedelta(days=rangeInDays)).strftime( '%Y-%m-%d %H:%m:%S') 
+if not showForm: 
+   query = "select EventStart,EventEnd,AssetID,EventType,NormalCapacity,AvailableCapacity  from outages where EventStart < ' %s ' and EventEnd > ' %s ' and EventStatus = 'OPEN'" % ( now, now) 
+else:
+   query = "select * from outages where EventStart > '%s' and EventEnd < '%s'" % ( t1, t2) 
 def extendQuery(query,text):
   if 'where' in query:
     query+=' and '+text
@@ -80,30 +94,28 @@ if AssetID:
 if MessageHeading: 
   query = extendQuery(query,"MessageHeading = %s " % str(MessageHeading))
 hanger.showquery(query)
-if sql: 
-  hanger.showquery(query)
+if any((EventStatus,AffectedUnit,EventType,AssetID,MessageHeading)): showForm = True
 
 cursor.execute(query)
 cols = map(lambda x: x[0], cursor.description) 
 #cols = ['AffectedUnitEIC','AssetType','AffectedUnit','DurationUncertainty','RelatedInformation','AssetId','EventType','NormalCapacity','AvailableCapacity','EventStatus','EventStart','EventEnd','Cause','FuelType','Participant_MarketParticipantID','MassageHeading']
-cols = ['AffectedUnit','AssetId','EventType','NormCap','AvailCap','EventStatus','EventStart','EventEnd','FuelType','MessageHeading']
+#cols = ['AffectedUnit','AssetId','EventType','NormCap','AvailCap','EventStatus','EventStart','EventEnd','FuelType','MessageHeading']
+cols = ['EventStart','EventEnd','AssetID','EventType','NormCap','AvailCap']
 rows = cursor.fetchall()
 cursor.close()
 d = []
 for row in rows:
-  d.append(( str(row['AffectedUnit']),
+  d.append(( 
+           str(row['EventStart']),
+           str(row['EventEnd']),
            str(row['AssetId']),
            str(row['EventType']),
            row['NormalCapacity'],
            row['AvailableCapacity'],
-           str(row['EventStatus']),
-           str(row['EventStart']),
-           str(row['EventEnd']),
-           str(row['FuelType']),
-           '<a href=outages.py?MessageHeading="REMIT Event ID 4168"> ' + str(row['MessageHeading'] + " </a>")
 ))
 rows = d
 
 
 hanger.bootstrap_table(rows,cols)
+print viewAll
 hanger.close()
