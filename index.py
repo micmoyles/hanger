2705fbe2a045806f40a655069869d3cc3a3aa1b2
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-08-31 21:24:59 +0000

    landing page split from dashboard

--- index.py
+++ index.py
@@ -4,6 +4,12 @@ import cgi, cgitb; cgitb.enable()
 import sys
 import hanger
 
-hanger.start('Home')
-hanger.h1('Home')
+hanger.start('Home', showMenu=False)
+print '''
+<div class='container'>
+<a href=dashboard.py><button type='button' class='btn btn-default btn-lg'>  DashBoard  </button></a>
+<a href='http://54.194.208.88:3000'> <button type='button' class='btn btn-default btn-lg'> Grafana </button></a>
+<a href='http://54.194.208.88:2812'> <button type='button' class='btn btn-default btn-lg'>  Monit  </button></a>
+</div>
+'''
 hanger.close()
