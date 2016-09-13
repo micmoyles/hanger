ae3bf9197d3c49c52c1fc677592c4b58a2a087a7
mmoyles <micmoyles@gmail.com>
2016-09-13 22:22:51 +0800

    new chart in dashboard

--- dashboard.js
+++ dashboard.js
@@ -17,4 +17,23 @@ window.onload = function () {
      ]
    });
    doughnutChart.render();
+
+	var dps2 = [{x: 1, y: 11}, {x: 2, y: 11}, {x: 3, y: 11}, {x: 4, y: 11}, {x: 5, y: 11}];   //dataPoints. 
+ 
+    var chart = new CanvasJS.Chart("lineChartContainer",{
+	title :{
+		text: "System Frequency"
+	},
+	axisX: {						
+		title: "Hertz"
+	},
+	axisY: {						
+		title: "min"
+	},
+	data: [{
+		type: "line",
+		dataPoints : dps
+	 }]
+});
+chart.render();
 }
